6d698ce367c90ebe121275c1af28a537b36e6bd8
miles retuta <milesretuta@gmail.com>
2015-11-19 18:59:17 -0800

    fixed angular routing

--- app.js
+++ app.js
@@ -1,32 +1,26 @@
+var myApp = angular.module('myApp', [
+  'ngRoute'
+]);
 
-var myApp = angular.module('myApp', ['ngView', 'routeProvider', 'locationProvider']);
+myApp.config(['$routeProvider',
+    function($routeProvider){
+        $routeProvider.
+            when('/', {
+            templateUrl: 'views/home.html'
+            })
+            // route for the sanitation page
+            .when('/sanitation', {
+                templateUrl: 'views/sanitation.html'
+            })
+            // route for the contact page
+            .when('/contact', {
+                templateUrl: 'views/contact.html'
+            })
+            .when('/about',{
+                templateUrl: 'views/about.html'
+            })
+            .otherwise({
+                redirectTo: '/'
+            });
 
-myApp.config(['routeProvider', 'locationProvider', 
-    function($routeProvider, $locationProvider){
-    $routeProvider
-        .when('/', {
-            templateUrl: '/views/home.html'
-        })
-        // route for the about page
-        .when('about', 
-        {
-            templateUrl: 'views/about.html',
-        })
-        // route for the sanitation page
-        .when('/sanitation', 
-        {
-            templateUrl: 'views/sanitation.html',
-        })
-        // route for the contact page
-        .when('/contact', 
-        {
-            templateUrl: 'views/contact.html',
-        })
-
-        .otherwise({
-            redirectTo: '/views/home.html',
-        });
-
-        $locationProvider.html5Mode(true);
-        $locationProvider.hashPrefix('!');
 }]);
